9da8670fc9b8115e2cffd895786632d08de436b1
Todd Smitala <tc.smitala@gmail.com>
2019-11-19 19:54:00 -0500

    final commit

--- LiveCode2/Program.cs
+++ LiveCode2/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace LiveCode2
+namespace LivecodeTwo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> sampleDict = new Dictionary<string, int>();
+            Dictionary<string, int> longRepeat(string sample)
+
+            {
+                string[] sampleArr = sample.Split();
+
+                for (int i = 0; i < sample.Length; i++)
+                {
+                    int k = 0;
+                    for (int j = 0; j < sample.Length; j++)
+                    {
+                        if (sampleArr[i] == sampleArr[j])
+                        {
+                            sampleDict.Add(sampleArr[i], k + 1);
+
+                        }
+
+                    }
+
+                }
+                return sampleDict;
+            }
+            Console.WriteLine("The string is 'aaa' and result is: " + longRepeat("aaa"));
+
         }
     }
 }
